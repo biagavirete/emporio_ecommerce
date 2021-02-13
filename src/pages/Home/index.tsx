3523633734d6f5cd3143a9c86957aa30aa229b0f
biagavirete <biagavirete@gmail.com>
2021-02-13 19:38:25 -0300

    feat: create home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,11 @@ import Header from '../../components/Header';
 import api from '../../services/api';
 import './styles.scss';
 import { IoBeer } from 'react-icons/io5';
+import { IBeer } from '../../types';
+import { IoCartOutline } from 'react-icons/io5';
 
 const Home = () => {
-  const [showProducts, setShowProducts] = useState([]);
+  const [beers, setBeers] = useState([]);
   const token = localStorage.getItem("token")
 
   useEffect(() => {
@@ -14,7 +16,7 @@ const Home = () => {
     }
 
     api.get('/beers', { headers: headers })
-      .then(response => console.log('beers', response.data))
+      .then(response => setBeers(response.data))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -22,8 +24,29 @@ const Home = () => {
     <>
       <Header />
       <div className="main-container">
-        <h1>Home</h1>
-        <IoBeer size={40} />
+        <div className="top-content">
+          <div className="icon-container">
+            <IoBeer size={40} />
+          </div>
+          <div className="text-container">
+            <h3>  Destaques no Empório</h3>
+          </div>
+        </div>
+        <div className="content">
+          {beers !== undefined && beers.map((beer: IBeer) => (
+            <div className="beer-container" key={beer.id}>
+              <div className="top">
+                <img src={beer.image} alt={beer.title} />
+                <p>{beer.description}</p>
+                <h3>{beer.title}</h3>
+              </div>
+              <div className="bottom">
+                <strong>{beer.price}</strong>
+                <button type="button">Adicionar <IoCartOutline size={20} /></button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface IBeer {
+  id: number;
+  description: string;
+  image: string;
+  title: string;
+  price: string;
+}
