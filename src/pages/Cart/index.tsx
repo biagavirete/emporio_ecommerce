9feaa17c86f31820159ebed3011a7464e41263f1
biagavirete <biagavirete@gmail.com>
2021-02-13 20:09:42 -0300

    feat: create cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Header from '../../components/Header';
+import { IoArrowForwardOutline } from 'react-icons/io5';
+import './styles.scss';
 
 const Cart = () => {
 
   return (
-    <h1>Cart</h1>
+    <>
+      <Header />
+      <div className="cart-container">
+        <div className="cart-content">
+          <div className="cart-header">
+            <h3>Meu carrinho</h3>
+            <IoArrowForwardOutline size={35} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
