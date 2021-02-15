3f6798a1535c634ddf64a267d614f47c756635b1
biagavirete <biagavirete@gmail.com>
2021-02-15 20:18:04 -0300

    feat: add header to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Routes from './routes';
 import './global.scss';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Provider store={store}>
+        <Header />
         <Routes />
       </Provider>
     </>
