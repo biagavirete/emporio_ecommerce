8d51ba791ae4a048e6f1ae0ce8d9a71167cd8f55
biagavirete <biagavirete@gmail.com>
2021-02-14 21:33:57 -0300

    feat: create add item to cart function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,11 @@ import Header from '../../components/Header';
 import api from '../../services/api';
 import './styles.scss';
 import { IoBeer } from 'react-icons/io5';
-import { IBeer } from '../../types';
 import { IoCartOutline } from 'react-icons/io5';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart, getProducts } from '../../store/ducks/cart/actions';
+import { IProduct } from '../../store/ducks/cart/types';
+import { Redirect } from 'react-router-dom';
 
 const Home = () => {
   const token = localStorage.getItem("token")
@@ -14,7 +15,6 @@ const Home = () => {
   const dispatch = useDispatch();
 
   const beers = useSelector((state: any) => state.cart.products)
-
 
   useEffect(() => {
     const headers = {
@@ -25,6 +25,11 @@ const Home = () => {
       .then(response => dispatch(getProducts(response.data)))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const addItemToCart = (item: IProduct) => {
+    dispatch(addToCart(item));
+    <Redirect to="/cart" />;
+  }
 
   return (
     <>
@@ -39,7 +44,7 @@ const Home = () => {
           </div>
         </div>
         <div className="content">
-          {beers !== undefined && beers.map((beer: IBeer) => (
+          {beers !== undefined && beers.map((beer: IProduct) => (
             <div className="beer-container" key={beer.id}>
               <div className="top">
                 <img src={beer.image} alt={beer.title} />
@@ -48,7 +53,7 @@ const Home = () => {
               </div>
               <div className="bottom">
                 <strong>{beer.price}</strong>
-                <button type="button" onClick={() => addToCart(beer)}>Adicionar <IoCartOutline size={20} /></button>
+                <button type="button" onClick={() => addItemToCart(beer)}>Adicionar <IoCartOutline size={20} /></button>
               </div>
             </div>
           ))}
