a02867276c1c8200950a32953468eeab42493a29
biagavirete <biagavirete@gmail.com>
2021-02-15 16:29:40 -0300

    feat: add validation access

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -35,33 +35,38 @@ const Home = () => {
 
   return (
     <>
-      <Header />
-      <div className="main-container">
-        <div className="top-content">
-          <div className="icon-container">
-            <IoBeer size={40} />
-          </div>
-          <div className="text-container">
-            <h3> Destaques no Empório</h3>
-          </div>
-        </div>
-        <div className="content">
-          {beers !== undefined && beers.map((beer: IProduct) => (
-            <div className="beer-container" key={beer.id}>
-              <div className="top">
-                <img src={beer.image} alt={beer.title} />
-                <p>{beer.description}</p>
-                <h3>{beer.title}</h3>
+      { token
+        ?
+        <>
+          <Header />
+          <div className="main-container">
+            <div className="top-content">
+              <div className="icon-container">
+                <IoBeer size={40} />
               </div>
-              <div className="bottom">
-                <strong>{beer.price}</strong>
-                <button type="button" onClick={() => addItemToCart(beer)}>Adicionar <IoCartOutline size={20} /></button>
-                {newCartItem && <Redirect to="/cart" />}
+              <div className="text-container">
+                <h3> Destaques no Empório</h3>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+            <div className="content">
+              {beers !== undefined && beers.map((beer: IProduct) => (
+                <div className="beer-container" key={beer.id}>
+                  <div className="top">
+                    <img src={beer.image} alt={beer.title} />
+                    <p>{beer.description}</p>
+                    <h3>{beer.title}</h3>
+                  </div>
+                  <div className="bottom">
+                    <strong>{beer.price}</strong>
+                    <button type="button" onClick={() => addItemToCart(beer)}>Adicionar <IoCartOutline size={20} /></button>
+                    {newCartItem && <Redirect to="/cart" />}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+        : <Redirect to="/" />}
     </>
   );
 }
